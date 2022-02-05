bd155fcb1c1236a42654d910638e766d3ec9c334
webgurusam <shyamghosh666@gmail.com>
2022-02-06 00:14:00 +0600

    updated

--- leap-year.js
+++ leap-year.js
@@ -1,14 +1,23 @@
 
 // 2022 % 4 = leap year hoiley vag ses 0 hobe
 
-// 
+function myLeapYear(year) {
+    if (year % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+const myYear = 2022;
+let leapYear = myLeapYear(myYear);
 
+console.log('Is it my leap year', leapYear);
 
+const herYear = 2023;
+leapYear = myLeapYear(herYear);
 
-
-
-
+console.log('Is it her leap year', leapYear);
 
 
 
