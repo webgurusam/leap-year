db423069c2b75efc7a4bcbaa10e588f656da0622
webgurusam <shyamghosh666@gmail.com>
2022-02-06 15:21:57 +0600

    updated

--- leap-year.js
+++ leap-year.js
@@ -1,30 +1,50 @@
 
-// 2022 % 4 = leap year hoiley vag ses 0 hobe
+// // 2022 % 4 = leap year hoiley vag ses 0 hobe (every 4 year)
+// // century waiz leap year 400 dia vag kore jantey hobe
 
-function myLeapYear(year) {
-    if (year % 2 == 0) {
+
+
+
+// function myLeapYear(year) {
+//     if (year % 2 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const myYear = 2022;
+// let leapYear = myLeapYear(myYear);
+
+// // console.log('Is it my leap year', leapYear);
+
+// const herYear = 2023;
+// leapYear = myLeapYear(herYear);
+
+// // console.log('Is it her leap year', leapYear);
+
+// // 0 theke alada hotey hobe (year % 100 != 0) khetrey
+
+
+function isItLeapYear(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
         return true;
     } else {
         return false;
     }
 }
 
-const myYear = 2022;
-let leapYear = myLeapYear(myYear);
 
-console.log('Is it my leap year', leapYear);
+const year = 2404;
 
-const herYear = 2023;
-leapYear = myLeapYear(herYear);
-
-console.log('Is it her leap year', leapYear);
+const leapYear = isItLeapYear(year);
 
 
-
-
-
-
-
+console.log('Is it leap year', leapYear);
 
 
 
